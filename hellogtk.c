4d618963eccbc7667e1381a59726c89d8dacabe6
Nathan Lanza <nathan@lanza.io>
2020-10-24 22:18:58 -0400

    Add but textfield from chapter 18

--- hellogtk.c
+++ hellogtk.c
@@ -1,5 +1,12 @@
 #include <gtk/gtk.h>
 
+GtkWidget *txt;
+
+void copy_text(GtkWidget *wid, gpointer ptr) {
+  const char *input = gtk_entry_get_text(GTK_ENTRY(txt));
+  gtk_label_set_text(GTK_LABEL(ptr), input);
+}
+
 void end_program(GtkWidget *wid, gpointer ptr) { gtk_main_quit(); }
 
 void count_button(GtkWidget *wid, gpointer ptr) {
@@ -21,8 +28,10 @@ int main(int argc, char *argv[]) {
 
   GtkWidget *lbl = gtk_label_new("My label");
 
-  GtkWidget *btn2 = gtk_button_new_with_label("Count button");
-  g_signal_connect(btn2, "clicked", G_CALLBACK(count_button), lbl);
+  GtkWidget *btn2 = gtk_button_new_with_label("Copy button");
+  g_signal_connect(btn2, "clicked", G_CALLBACK(copy_text), lbl);
+  
+  txt = gtk_entry_new();
 
   // GtkWidget *box = gtk_vbox_new(false, 5);
   // gtk_box_pack_start(GTK_BOX(box), btn2, true, true, 0);
@@ -33,7 +42,9 @@ int main(int argc, char *argv[]) {
   GtkWidget *tbl = gtk_table_new(2, 2, TRUE);
   gtk_table_attach_defaults(GTK_TABLE(tbl), lbl, 0, 1, 0, 1);
   gtk_table_attach_defaults(GTK_TABLE(tbl), btn2, 1, 2, 0, 1);
-  gtk_table_attach_defaults(GTK_TABLE(tbl), btn, 0, 2, 1, 2);
+  gtk_table_attach_defaults(GTK_TABLE(tbl), btn, 0, 1, 1, 2);
+  gtk_table_attach_defaults(GTK_TABLE(tbl), btn, 0, 1, 1, 2);
+  gtk_table_attach_defaults(GTK_TABLE(tbl), txt, 1, 2, 1, 2);
   gtk_container_add(GTK_CONTAINER(win), tbl);
 
   gtk_widget_show_all(win);
